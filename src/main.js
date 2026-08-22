952b66c752b285ee836828d01a7c9426248ebae0
latissimus <flrn.rau@icloud.com>
2026-08-22 17:55:34 +0200

    u

--- src/main.js
+++ src/main.js
@@ -1240,7 +1240,7 @@ async function renderRoute() {
     const openEntry = (type, foodKind = null) => openDexEntryEditor({
       type, foodKind, userId: foodOwnerId, rootKey: 'food-log', onSaved: refresh,
     });
-    mountCategoryChrome(view, route, 'FoodDex', {
+    mountCategoryChrome(view, route, 'FOODDEX', {
       pageLookScope: route, pageLookPattern: 'triangles',
       meta: `${children.length} Unter-Dex`,
       onAddNote: () => openEntry('note'),
